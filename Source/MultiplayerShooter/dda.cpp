d161747c5b1c04fbc74adb7aba6bc2de9deeddc8
Shush <shushanik@neographgames.com>
2024-11-27 17:26:53 +0300

    Removed unnessary files

--- Source/MultiplayerShooter/dda.cpp
+++
@@ -1,2 +0,0 @@
-#include "C:\Users\Shush\Desktop\MultiplayerShooter\Intermediate\Build\Win64\x64\MultiplayerShooterEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
-#include "dda.h"
--- Source/MultiplayerShooter/dda.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class dda
-{
-public:
-	
-};
